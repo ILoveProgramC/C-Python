f2549273c31fc082040322e1fedd6f7547648de1
ILoveProgramC <123211731+ILoveProgramC@users.noreply.github.com>
2023-06-10 15:54:48 +0200

    C-Pajton.py

--- C pokrenut u Pajtonu/C-Pajton.py
+++ C pokrenut u Pajtonu/C-Pajton.py
@@ -3,7 +3,7 @@ from ctypes import *
 # import ctypes   --> Ako koristimo drugi nacin za odredjivanje putanje potrebno je importovati
 
 #Prvi nacin definisanja putanje:
-so_file = "/Users/VasaPutanjaDoDatoteke/C pokrenut u Pajtonu/C-program.so" #definisite svoju putanju na vasem racunaru
+so_file = "/Users/PutanjaDoDatoteke/C pokrenut u Pajtonu/C-program.so" #definiste svoju putanju na vasem racunaru
 
 #Drugi nacin definisanja putanje: 
 """
@@ -14,14 +14,14 @@ osnovna_funkcija = ctypes.CDLL(putanja_do_izvrsne_datoteke)
 """
 
 osnovna_funkcija = CDLL(so_file)
-
 print("Tip: ",type(osnovna_funkcija))
 osnovna_funkcija.ispis() # ispis na standardnom izlazu
-
 print("Funkcija kvadrat: ", osnovna_funkcija.kvadrat(5)) #s obzirom da je u pitanju funkcija u C-u koja jednostavno vraca
 #kvadrat broja koji navedemo u argumentu, rezultat ce biti prikazan u Python/u u tipu podatka int
 #Ne smijemo koristiti realan broj jer u C funkciji se koristi podatak tipa int
 
 osnovna_funkcija.unos()  #Funkcija kojom se vrsi unos sa tastature i ispis
+
+osnovna_funkcija.main() #Glavna funkcija u C-u, u njoj sam ubacio kreiranje tekstualne datoteke.
 
 
