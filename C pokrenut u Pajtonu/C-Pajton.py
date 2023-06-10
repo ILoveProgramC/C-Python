6c9b224994c4a33a289c30ceef1cc9436e0b482b
ILoveProgramC <123211731+ILoveProgramC@users.noreply.github.com>
2023-06-10 16:55:06 +0200

    C-Pajton.py

--- C pokrenut u Pajtonu/C-Pajton.py
+++ C pokrenut u Pajtonu/C-Pajton.py
@@ -4,16 +4,15 @@ from ctypes import *
 
 #Prvi nacin definisanja putanje:
 so_file = "/Users/PutanjaDoDatoteke/C pokrenut u Pajtonu/C-program.so" #definiste svoju putanju na vasem racunaru
+osnovna_funkcija = CDLL(so_file)
 
 #Drugi nacin definisanja putanje: 
 """
 trenutna_putanja = os.path.abspath(os.path.dirname(__file__))
 putanja_do_izvrsne_datoteke = os.path.join(trenutna_putanja, 'C-program.so')
-
 osnovna_funkcija = ctypes.CDLL(putanja_do_izvrsne_datoteke)
 """
 
-osnovna_funkcija = CDLL(so_file)
 print("Tip: ",type(osnovna_funkcija))
 osnovna_funkcija.ispis() # ispis na standardnom izlazu
 print("Funkcija kvadrat: ", osnovna_funkcija.kvadrat(5)) #s obzirom da je u pitanju funkcija u C-u koja jednostavno vraca
